4245eb7fc066814152666ec6736c4800e41409fa
katafalk <katafalk2009@gmail.com>
2021-08-10 21:04:51 +0300

    All tests ready.

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import requests
 import time
+from functools import wraps
 
 FILMS_URL = 'https://swapi.dev/api/films/'
 PEOPLE_URL = 'https://swapi.dev/api/people/'
 
 
-class DATA():
+class DATA:
+    """Class to represent an object data received
+     from api. Same for people and films."""
     def __init__(self, attributes):
         for key in attributes:
             setattr(self, key, attributes[key])
-    def __str__(self):
-        return [i for i in self.__dict__]
 
+    def __str__(self):
+        if hasattr(self, 'name'):
+            return self.name
+        elif hasattr(self, 'title'):
+            return self.title
+        else:
+            return None
 
 
 def check_time(f):
+    """Decorate function to check time of its execution."""
+    @wraps(f)
     def wrapper(*args, **kwargs):
         start = time.time()
         result = f(*args, **kwargs)
@@ -26,24 +36,43 @@ def check_time(f):
 
 @check_time
 def get_object_data(url):
+    """Return response of GET request to API as an object of class DATA."""
     response = requests.get(url).json()
     return DATA(response)
 
 
 @check_time
 def get_page_data(url):
+    """Return response of GET request to API decoded from json to a dict."""
     response = requests.get(url)
     return response.json()
 
 
 @check_time
+def get_page_status_code(url):
+    """Return status code of GET request to API.    """
+    response = requests.get(url)
+    return response.status_code
+
+
+@check_time
 def get_all_data(url):
+    """Return all objects data from a GET request
+     to a resource as object of DATA class."""
     response = requests.get(url)
     data = response.json()
     count = data['count']
     return [get_object_data(url+str(i)) for i in range(1, count+1)]
 
 
+def main():
+    people = get_all_data(PEOPLE_URL)
+    films = get_all_data(FILMS_URL)
+    for person in people:
+        print(person)
+    for film in films:
+        print(film)
 
-for p in get_all_data(PEOPLE_URL):
-    print(p)
+
+if __name__ == '__main__':
+    main()
--- tests.py
+++ tests.py
@@ -1,5 +1,192 @@
-import pytest
 import main
-def test_resource_code():
-    assert main.get_page_data(main.FILMS_URL) == 200
+import logging
 
+
+logger = logging.FileHandler("x.log")
+
+
+def test_resource_code_people():
+    assert main.get_page_status_code(main.PEOPLE_URL) == 200
+
+
+def test_resource_data_people():
+    response = main.get_page_data(main.PEOPLE_URL)
+    assert set(response.keys()) == {'count', 'next', 'previous', 'results'}
+
+
+def test_resource_next_films():
+    response = main.get_page_data(main.PEOPLE_URL)
+    next_ = response['next']
+    print(next_)
+    assert main.get_page_status_code(next_) == 200
+
+
+def test_id_code_people():
+    assert main.get_page_status_code(main.PEOPLE_URL + '1') == 200
+
+
+def test_id_data_people():
+    response = main.get_page_data(main.PEOPLE_URL + '1')
+    assert set(response.keys()) == {'name', 'birth_year', 'eye_color',
+                                    'gender', 'hair_color', 'height', 'mass',
+                                    'skin_color', 'homeworld', 'films',
+                                    'species', 'starships', 'vehicles',
+                                    'url', 'created', 'edited'}
+
+
+def test_id_code_people_last():
+    response = main.get_page_data(main.PEOPLE_URL)
+    count = response['count']
+    assert main.get_page_status_code(main.PEOPLE_URL + str(count)) == 200
+
+
+def test_id_data_people_last():
+    response = main.get_page_data(main.PEOPLE_URL)
+    count = response['count']
+    response_last = main.get_page_data(main.PEOPLE_URL + str(count))
+    assert set(response_last.keys()) == {'name', 'birth_year', 'eye_color',
+                                         'gender', 'hair_color', 'height',
+                                         'mass', 'skin_color', 'homeworld',
+                                         'films', 'species', 'starships',
+                                         'vehicles', 'url', 'created',
+                                         'edited'}
+
+
+def test_schema_status_people():
+    assert main.get_page_status_code(main.PEOPLE_URL + 'schema') == 200
+
+
+def test_search_status_people():
+    assert main.get_page_status_code(main.PEOPLE_URL + '?search=yoda') == 200
+
+
+def test_search_data_people():
+    response = main.get_page_data(main.PEOPLE_URL + '?search=yoda')
+    assert response['results'][0]['name'] == 'Yoda'
+
+
+def test_search_status_blablabla_people():
+    assert main.get_page_status_code(
+        main.PEOPLE_URL + '?search=blablabla') == 200
+
+
+def test_search_data_blablabla_people():
+    response = main.get_page_data(main.PEOPLE_URL + '?search=blablabla')
+    assert response['results'] == []
+
+
+def test_wookie_status_people():
+    assert main.get_page_status_code(
+        main.PEOPLE_URL + '1' + '?format=wookiee') == 200
+
+
+def test_wookie_data_people():
+    assert len(main.get_page_data(main.PEOPLE_URL + '1' + '?format=wookiee')) \
+           == len(main.get_page_data(main.PEOPLE_URL + '1'))
+
+
+def test_ivalid_resource():
+    assert main.get_page_status_code('https://swapi.dev/api/person') == 404
+
+
+def test_zero_id_code_people():
+    assert main.get_page_status_code(main.PEOPLE_URL + '0') == 404
+
+
+def test_id_code_people_last_plus1():
+    response = main.get_page_data(main.PEOPLE_URL)
+    count = response['count']
+    assert main.get_page_status_code(main.PEOPLE_URL + str(count + 1)) == 404
+
+
+def test_invalid_status_people():
+    assert main.get_page_status_code(
+        main.PEOPLE_URL + '1' + '?format=bookiee') == 404
+
+
+def test_resource_code_films():
+    assert main.get_page_status_code(main.FILMS_URL) == 200
+
+
+def test_resource_data_films():
+    response = main.get_page_data(main.FILMS_URL)
+    assert set(response.keys()) == {'count', 'next', 'previous', 'results'}
+
+
+def test_id_code_films():
+    assert main.get_page_status_code(main.FILMS_URL + '1') == 200
+
+
+def test_id_data_films():
+    response = main.get_page_data(main.FILMS_URL + '1')
+    assert set(response.keys()) == {'title', 'episode_id', 'opening_crawl',
+                                    'director', 'producer', 'release_date',
+                                    'species', 'starships', 'vehicles',
+                                    'characters', 'planets', 'url', 'created',
+                                    'edited'}
+
+
+def test_id_code_films_last():
+    response = main.get_page_data(main.FILMS_URL)
+    count = response['count']
+    assert main.get_page_status_code(main.FILMS_URL + str(count)) == 200
+
+
+def test_id_data_films_last():
+    response = main.get_page_data(main.FILMS_URL)
+    count = response['count']
+    response_last = main.get_page_data(main.FILMS_URL + str(count))
+    assert set(response_last.keys()) == {'title', 'episode_id',
+                                         'opening_crawl', 'director',
+                                         'producer', 'release_date',
+                                         'species', 'starships', 'vehicles',
+                                         'characters', 'planets', 'url',
+                                         'created', 'edited'}
+
+
+def test_schema_status_films():
+    assert main.get_page_status_code(main.FILMS_URL + 'schema') == 200
+
+
+def test_search_status_films():
+    assert main.get_page_status_code(main.FILMS_URL + '?search=hope') == 200
+
+
+def test_search_data_films():
+    response = main.get_page_data(main.FILMS_URL + '?search=hope')
+    assert response['results'][0]['title'] == 'A New Hope'
+
+
+def test_search_status_blablabla_films():
+    assert main.get_page_status_code(
+        main.FILMS_URL + '?search=blablabla') == 200
+
+
+def test_search_data_blablabla_films():
+    response = main.get_page_data(main.FILMS_URL + '?search=blablabla')
+    assert response['results'] == []
+
+
+def test_wookie_status_films():
+    assert main.get_page_status_code(
+        main.FILMS_URL + '1' + '?format=wookiee') == 200
+
+
+def test_wookie_data_films():
+    assert len(main.get_page_data(main.FILMS_URL + '1' + '?format=wookiee')) \
+           == len(main.get_page_data(main.FILMS_URL + '1'))
+
+
+def test_zero_id_code_films():
+    assert main.get_page_status_code(main.FILMS_URL + '0') == 404
+
+
+def test_id_code_films_last_plus1():
+    response = main.get_page_data(main.FILMS_URL)
+    count = response['count']
+    assert main.get_page_status_code(main.FILMS_URL + str(count + 1)) == 404
+
+
+def test_invalid_status_films():
+    assert main.get_page_status_code(
+        main.FILMS_URL + '1' + '?format=bookiee') == 404
